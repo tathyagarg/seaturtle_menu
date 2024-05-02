f4330b45a68172f085764335f40d695dd97c897d
Tathya Garg <tathya.garg@gmail.com>
2024-05-02 23:37:34 +0530

    Updated version info

--- seaturtle_menu/__init__.py
+++ seaturtle_menu/__init__.py
@@ -3,10 +3,10 @@ from .consts import *
 from .menu import *
 from .utils import *
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 
 __all__ = ['Bullets', 'Bulletable', 'LOWERCASE_ALPHABET', 'UPPERCASE_ALPHABET', 'DIGITS', 'Menu']
-__version__ = '.'.join(map(str, VERSION)) + '-dev'
+__version__ = '.'.join(map(str, VERSION)) + '.dev0'
 __license__ = 'MIT'
 __author__ = "Tathya Garg"
 __email__ = "tathya.garg@gmail.com"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0.dev0'
+VERSION = '0.1.1.dev0'
 DESCRIPTION = 'Make effective terminal menus quickly and concisely.'
 
 setup(
